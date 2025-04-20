7db57527e9848fe8d6ce32391dd1fa73fe61fda1
Muhammed Shittu <devshittu@gmail.com>
2025-04-20 14:44:17 +0100

    replaced & refactored to allow pipx installation

--- uninstall.sh
+++ uninstall.sh
@@ -1,33 +1,3 @@
-# #!/bin/bash
-
-# # Uninstall flatten tool
-# set -e
-
-# INSTALL_DIR="/usr/local/bin"
-# SCRIPT_NAME="flatten"
-# PLUGIN_DIR="/usr/local/share/flatten/plugins"
-# TEMPLATE_DIR="/usr/local/share/flatten/templates"
-
-# # Remove script
-# if [ -f "$INSTALL_DIR/$SCRIPT_NAME" ]; then
-#     echo "Removing $SCRIPT_NAME from $INSTALL_DIR..."
-#     sudo rm "$INSTALL_DIR/$SCRIPT_NAME"
-# else
-#     echo "Warning: $SCRIPT_NAME not found in $INSTALL_DIR."
-# fi
-
-# # Remove plugins and templates
-# if [ -d "$PLUGIN_DIR" ]; then
-#     echo "Removing $PLUGIN_DIR..."
-#     sudo rm -rf "$PLUGIN_DIR"
-# fi
-# if [ -d "$TEMPLATE_DIR" ]; then
-#     echo "Removing $TEMPLATE_DIR..."
-#     sudo rm -rf "$TEMPLATE_DIR"
-# fi
-
-# echo "Uninstallation successful."
-
 #!/bin/bash
 
 # Uninstall flatten tool (pipx, local, or global)
@@ -121,3 +91,5 @@ else
 
     echo "Global uninstallation successful."
 fi
+
+# File path: ./install.sh
